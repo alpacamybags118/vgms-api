e25cec381d1a3788fdd97259d09dc92d8c10949f
Kyle Robertson <jsakyle@gmail.com>
2018-02-07 22:35:46 -0500

    added game model

--- src/Model/Game.php
+++ src/Model/Game.php
@@ -5,5 +5,43 @@ namespace App\Model;
 
 class Game
 {
+    private $Id;
+    private $Name;
+    private $System;
+    private $Condition;
+
+    public function __construct()
+    {
+        $this->Id = uniqid();
+    }
+
+    public function GetId()
+    {
+        return $this->Id;
+    }
+    public function GetName()
+    {
+        return $this->Name;
+    }
+    public function GetSystem()
+    {
+        return $this->System;
+    }
+    public function GetCondition()
+    {
+        return $this->Condition;
+    }
+    public function SetName(string $name)
+    {
+        $this->Name = $name;
+    }
+    public function SetSystem(string $system)
+    {
+        $this->System = $system;
+    }
+    public function SetCondition(string $condition)
+    {
+        $this->Condition = $condition;
+    }
 
 }
